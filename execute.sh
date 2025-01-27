b5b93a2b767b17b794c785cca80499acb01939bf
cwpace97 <cwpace97@gmail.com>
2025-01-27 16:36:46 -0700

    refactor repos

--- execute.sh
+++
@@ -1,5 +0,0 @@
-source /home/cwp97pi/code/corduroy/.env/bin/activate
-# python /home/cwp97pi/code/corduroy/local_funcs/scraper_copper.py
-# python /home/cwp97pi/code/corduroy/local_funcs/scraper_loveland.py
-# python /home/cwp97pi/code/corduroy/local_funcs/scraper_winterpark.py
-deactivate
